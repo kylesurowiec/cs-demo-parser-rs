ebcef85332c57758b3a95a504f6b9b4e2ad45555
Kyle Surowiec <kylesurowiec@me.com>
2025-07-03 01:42:54 -0600

    Add common structs and equipment types

---
+++ demoinfocs-rs/src/common/mod.rs
@@ -0,0 +1,11 @@
+// Common data structures shared across the parser.
+
+mod equipment;
+mod grenade;
+mod hostage;
+mod player;
+
+pub use equipment::*;
+pub use grenade::*;
+pub use hostage::*;
+pub use player::*;
