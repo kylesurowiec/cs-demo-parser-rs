9a614eebb29e29d0f1dd887c6601e462fa2a70e5
Kyle Surowiec <kylesurowiec@me.com>
2025-07-04 17:09:34 -0600

    Remove old Go sources and update docs (#48)

--- demoinfocs-rs/src/sendtables/serverclass.rs
+++ demoinfocs-rs/src/sendtables/serverclass.rs
@@ -126,7 +126,12 @@ impl fmt::Display for ServerClass {
         write!(
             f,
             "serverClass: id={} name={}\n\tdataTableId={}\n\tdataTableName={}\n\tbaseClasses:\n\t\t{}\n\tproperties:\n\t\t{}",
-            self.id, self.name, self.data_table_id, self.data_table_name, base, props
+            self.id,
+            self.name,
+            self.data_table_id,
+            self.data_table_name,
+            base,
+            props,
         )
     }
 }
